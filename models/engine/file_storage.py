3db1b075e8ec2e27887f75250ea4cc1dc27b8ee9
abdulkarim <abdulkarimkrim6789@gmail.com>
2023-03-20 18:12:59 +0100

    add new method to delete an instance of a model

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,8 +8,17 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        if cls:
+            cls_objects = {}
+            for key, value in FileStorage.__objects.items():
+                new = value.to_dict()
+                state = new["__class__"]
+                if cls.__name__ == state:
+                    cls_objects[key] = value
+            return cls_objects
+
         return FileStorage.__objects
 
     def new(self, obj):
@@ -45,6 +54,16 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """ Delete a model from FileStorage
+            obj (obj): model to delete
+        """
+        if obj:
+            for key, value in FileStorage.__objects.items():
+                if value == obj:
+                    break
+            del FileStorage.__objects[key]
