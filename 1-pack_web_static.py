0454d5ebf800ec45cd5056c3e4ceda059a7efdb4
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-04-06 16:06:29 +0100

    create .tgz archived

---
+++ 1-pack_web_static.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Generates a .tgz archive from the contents of the web_static
+    folder of the AirBnB Clone repo using do_pack function
+    """
+from fabric.api import local, runs_once
+from datetime import datetime
+import os
+
+
+def do_pack():
+    """Create a .tgz archive"""
+    if os.path.isdir('versions'):
+        pass
+    else:
+        os.mkdir('versions')
+    try:
+        time = datetime.now()
+        file = "versions/web_static{}{}{}{}{}{}.tgz".format(
+                time.year,
+                time.month,
+                time.day,
+                time.hour,
+                time.minute,
+                time.second
+                )
+        print(f"Packing web_static to {file}")
+        local(f"tar -czvf {file} web_static/")
+        file_size = os.stat(file).st_size
+        print(f"web_static packed: {file} -> {file_size} Bytes")
+    except Exception:
+        file = None
+        return file
