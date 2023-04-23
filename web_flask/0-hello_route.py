32bc338536ea24e7d320c8191955feec79b5f192
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-04-23 13:41:40 +0100

    use the struct_slashes option

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """This script creates a Flask web application listening on 0.0.0.0"""
 from flask import Flask
-
+"""Instance of the application"""
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 @app.route("/")
 def hello():
