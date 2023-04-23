5d85cb82bb3cd53986397caeddcc75a0ad5fdf71
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-04-23 12:33:05 +0100

    run app on port 0.0.0.0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This script creates a Flask web application listening on 0.0.0.0"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
