335f807d4af5ea62cabfcf382ade610498dc942e
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-04-23 13:43:12 +0100

    update port and function name

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """This script creates a Flask web application listening on 0.0.0.0"""
 from flask import Flask
-"""Instance of the application"""
 app = Flask(__name__)
+"""Instance of the application"""
 app.url_map.strict_slashes = False
 
 @app.route("/")
-def hello():
+def index():
     """Display Hello HBNB"""
     return "Hello HBNB!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port='5000')
