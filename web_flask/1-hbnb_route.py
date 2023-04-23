230daef280e1081f9340828423c7214d04c50198
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-04-23 13:51:39 +0100

    Display hbnb page

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This script creates a Flask web application listening on 0.0.0.0"""
+from flask import Flask
+app = Flask(__name__)
+"""Instance of the application"""
+app.url_map.strict_slashes = False
+
+@app.route("/")
+def index():
+    """Display home page"""
+    return "Hello HBNB!"
+
+@app.route("/hbnb")
+def hbnb():
+    """Display hbnb page"""
+    return "HBNB"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
